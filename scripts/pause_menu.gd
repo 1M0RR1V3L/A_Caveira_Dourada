8995d465a2af6e7f15677673015f0d34dc1022e8
Lucas <lsxyz2017@gmail.com>
2023-12-07 15:42:15 -0300

    added pause-menu button, added new folder for themes

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+@onready var resume_button = $menu_screen/Resume_button
+
+func _ready():
+	visible = false
+	
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		resume_button.grab_focus()
+
+func _on_resume_button_pressed():
+	get_tree().paused = false
+	visible = false
+	
+
+func _on_back_menu_button_pressed():
+	get_tree().quit()
