970846b45f33dc6d595a377b1e79f7313dc49444
Lucas <lsxyz2017@gmail.com>
2023-12-08 07:53:52 -0300

    added new dialogue, menu correction

--- scripts/introduction.gd
+++ scripts/introduction.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
-func _ready():
+@onready var start_button = $VBoxContainer/StartButton
 
+			
+func _ready():
+		
 		var resource = preload("res://scripts/dialogue/introduction.dialogue")
 		DialogueManager.show_example_dialogue_balloon(\
 		\
 		resource
 		)
-
-func _on_skip_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/cemetery.tscn")
-
+		
+func _unhandled_input(event):
+	if event.is_action_pressed("Enter"):
+		get_tree().change_scene_to_file("res://scenes/cemetery.tscn")
+		
+func _on_start_button_pressed():
+		get_tree().change_scene_to_file("res://scenes/cemetery.tscn")
